37250a64da256a6f9d2e86a2537eaa7bec640279
ChanOh Jeong <pooo1002@naver.com>
2019-12-20 17:58:45 +0900

    Modified chat_bot

--- python/telegram_chatbot/app.py
+++ python/telegram_chatbot/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from decouple import config
 import requests
+import random
 
 app = Flask(__name__)
 
@@ -29,14 +30,21 @@ def send():
 
 @app.route(f'/{token}', methods=["POST"])  # 어렵게 만든 이유 : 다른사람이 악용하는것을 막는것.
 def telegram():
-    # chat_id = request.get_json.[][][]
-    # if text == "로또" or "roto":
-    #     text
-    # elif
-    # elif
-    # elif
+    wholeJ = request.get_json()
+    text = wholeJ['message']['text']
+    chat_id = wholeJ['message']['chat']['id']
+    if text == "안녕":
+        return_text = "안녕하세요!"
+    elif text == "로또":
+        numbers = range(1, 46)
+        return_text = random.sample(numbers, 6)  # numbers안에서 6개 무작위 뽑아줌
+    else:
+        return_text = "뭐임마?"
+    requests.get(
+        f'{url}{token}/sendMessage?chat_id={chat_id}&text={return_text}')
     return "ok", 200  # Telegram은 요청에 대한 응답이 제대로 수행될때까지 반복하기 때문에 정상응답값 200을 return
     # telegram과 연동이 되어 요청받고 return값으로 200 응답
+
 
 if __name__ == ("__main__"):
     app.run(debug=True)
--- python/telegram_chatbot/webhook.py
+++ python/telegram_chatbot/webhook.py
@@ -3,10 +3,11 @@ import requests
 
 token = config("TELEGRAM_BOT_TOKEN")
 url = "https://api.telegram.org/bot"
-ngrok_url = "https://0ba397b6.ngrok.io"
+pw_url = "https://chanohjeong.pythonanywhere.com/"
 
 
-data = requests.get(f'{url}{token}/setwebhook?url={ngrok_url}/{token}')
+data = requests.get(f'{url}{token}/setwebhook?url={pw_url}{token}')
+print(data.text)
 # 텔레그램 챗봇에 요청이 들어오면 ngrok주소로 가세요~라는 뜻 webhook을 통해 우리서버와 telegram서버가 연동이 됨(로컬서버기때문에 ngrok사용)
 # Telegram Server -> WebHook -> Local Flask
-print(data.text)
+# 주소가 변경될때만 주소바꿔서 실행시키면 됨
